0d2dcdddf82731da64e0ec3135903ab54b7e567f
Sebastian Dowell <142616573+SebastianAPCS@users.noreply.github.com>
2024-04-28 18:01:51 -0700

    this probably wont work

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -1,5 +1,6 @@
-public class LightBoard
-{
+import java.util.*;
+
+public class LightBoard {
   /** The lights on the board, where true represents on and false represents off.
    */
   private boolean[][] lights;
@@ -8,21 +9,44 @@ public class LightBoard
    * Precondition: numRows > 0, numCols > 0
    * Postcondition: each light has a 40% probability of being set to on.
    */
-  public LightBoard(int numRows, int numCols)
-  {
-    /* to be implemented in part (a) */
+  public LightBoard(int numRows, int numCols) {
+    lights = new boolean[numRows][numCols];
 
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numCols; j++) {
+        lights[i][j] = Math.random() <= 0.4;
+      }
+    }
   }
 
   /** Evaluates a light in row index row and column index col and returns a status
    *  as described in part (b).
    *  Precondition: row and col are valid indexes in lights.
    */
-  public boolean evaluateLight(int row, int col)
-  {
-    /* to be implemented in part (b) */
-   
- 
+  public boolean evaluateLight(int row, int col) {
+    if (lights[row][col]) {
+      int numLightsInColumn = 0;
+      for (int i = 0; i < lights.length; i++) {
+        if (lights[i][col]) {
+          numLightsInColumn ++;
+        }
+      }
+      if (numLightsInColumn % 2 == 0) {
+        return false;
+      }
+    } else {
+      int numLightsInColumn = 0;
+      for (int i = 0; i < lights.length; i++) {
+        if (lights[i][col]) {
+          numLightsInColumn ++;
+        }
+      }
+      if (numLightsInColumn % 3 == 0) {
+        return true;
+      }
+    }
+
+    return lights[row][col];
   }
   public boolean[][] getLights()
   {
